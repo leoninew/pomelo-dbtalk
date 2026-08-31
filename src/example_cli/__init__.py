32d2be4401b93d23d1a7671e0c23b4dbc72c211c
leoninew <leoninewang@outlook.com>
2026-08-31 04:32:10 +0100

    chore: remove example CLI template

--- src/example_cli/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Extensible Click CLI template."""
-
-__version__ = "0.1.0"
--- src/example_cli/commands/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Built-in CLI commands."""
-
-from example_cli.commands.greet import greet
-from example_cli.commands.status import status
-
-__all__ = ["greet", "status"]
